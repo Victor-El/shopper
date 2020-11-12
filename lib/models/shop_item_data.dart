e748b1146e5649b4e5521ea7e3334874b10970e7
Victor-El <victorelezua@gmail.com>
2020-11-12 16:59:23 +0100

    refactor: modify shop_item_data to support database

--- lib/models/shop_item_data.dart
+++ lib/models/shop_item_data.dart
@@ -1,9 +1,32 @@
+import 'package:shopper/data/local/db_contract.dart';
+
 class ShopItemData {
+  int _id;
   String _item;
   double _price;
   bool _checked;
 
   ShopItemData(this._item, this._price, this._checked);
+
+
+  ShopItemData.fromMap(Map<String, dynamic> shopItemDataMap) {
+    this._id = shopItemDataMap[ID] as int;
+    this._item = shopItemDataMap[ITEM_COLUMN] as String;
+    this._price = shopItemDataMap[PRICE_COLUMN] as double;
+    this._checked = (shopItemDataMap[CHECKED_COLUMN] as int) == 1;
+  }
+
+  Map<String, dynamic>toMap() {
+    return {
+      ITEM_COLUMN: this._item,
+      PRICE_COLUMN: this._price,
+      CHECKED_COLUMN: this._checked == true ? 1 : 0
+    };
+  }
+
+  int get id => _id;
+
+  set id(int value) => _id = value;
 
   String get item => _item;
 
